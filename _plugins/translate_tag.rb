9bc59484d275405529abf0b90a57720d097dda3a
tt-admin <admin@traveltripper.io>
2016-10-06 09:25:44 -0400

    Update Plugins

--- _plugins/translate_tag.rb
+++ _plugins/translate_tag.rb
@@ -5,7 +5,7 @@ module Jekyll
     def initialize(tag_name, token, *args)
       super
       params = token.to_s.strip.split(',')
-      @token = params[0].strip
+      @token = eval(params[0].strip)
       @locale = params[1].to_s.strip
       @locale = nil if @locale == ''
     end
@@ -13,7 +13,7 @@ module Jekyll
     def render(context)
       site = context.registers[:site]
       load_translations(site.source)
-      locale = @locale || site.active_lang || site.default_lang || 'en'
+      locale = context[@locale] || @locale || site.active_lang || site.default_lang || 'en'
       I18n.available_locales = site.languages || [site.default_lang || 'en']
 
       I18n.t @token, locale: locale
